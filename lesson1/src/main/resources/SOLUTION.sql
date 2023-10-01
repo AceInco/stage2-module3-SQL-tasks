aa281225a963ca74047d917c5b7cd39c8526996b
Mikita <vishnevkiy.nikita@gmail.com>
2023-10-01 03:57:02 +0300

    1234

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,2 +1,37 @@
+CREATE TABLE Student(
+    id bigint PRIMARY KEY,
+    name varchar,
+    birthday date,
+    groupnumber int
+);
+CREATE TABLE Subject(
+    id bigint PRIMARY KEY,
+    name varchar,
+    description varchar,
+    grade int
+);
+CREATE TABLE PaymentType(
+    id bigint PRIMARY KEY,
+    name varchar
+);
+CREATE TABLE Payment(
+    id bigint PRIMARY KEY,
+    type_id bigint,
+    amount decimal,
+    student_id bigint,
+    payment_date datetime,
+    FOREIGN KEY (type_id) references PaymentType (id),
+    FOREIGN KEY (student_id) references Student (id)
+);
+CREATE TABLE Mark(
+    id bigint PRIMARY KEY,
+    student_id bigint,
+    subject_id bigint,
+    mark int,
+    FOREIGN KEY (student_id) references Student (id),
+    FOREIGN KEY (subject_id) references Subject (id)
+);
+
+
 
 
---
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -0,0 +1,14 @@
+INSERT INTO DEFAULT.PUBLIC.STUDENT (name, GROUPNUMBER)
+VALUES
+('Chris', 1),
+('Carl', 1),
+('Oliver', 2),
+('James', 2),
+('Lucas', 2),
+('Henry', 2),
+('Jacob', 3),
+('Logan', 3),
+('Mena', 4),
+('ALESHA', 5);
+
+
