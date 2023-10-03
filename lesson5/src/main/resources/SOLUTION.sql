61fd1287a6b16de29b409e58bf21524a03826664
Mikita <vishnevkiy.nikita@gmail.com>
2023-10-03 16:52:19 +0300

    1234

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
 SELECT * FROM payment WHERE AMOUNT >= 500;
-SELECT * FROM student WHERE DATEADD(YEAR, -20, CURRENT_DATE) > birthday;
-SELECT * FROM student WHERE groupnumber = 10 AND DATEADD(YEAR, 20, CURRENT_DATE) > birthday;
+SELECT * FROM student WHERE DATEADD(YEAR, -20, CURRENT_DATE) >= birthday;
+SELECT * FROM student WHERE groupnumber = 10 AND DATEADD(YEAR, 20, CURRENT_DATE) <= birthday;
 SELECT * FROM student WHERE name = 'Mike' OR groupnumber IN (4, 5, 6);
-SELECT * FROM payment WHERE payment_date <= DATEADD(MONTH, -8, CURRENT_DATE);
+SELECT * FROM payment WHERE payment_date >= DATEADD(MONTH, -8, CURRENT_DATE);
 SELECT * FROM student WHERE LEFT(name, 1) = 'A';
 SELECT * FROM student WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9);
