ac86e5825793a72d4ffeff13f0f33043e0d00709
Mikita <vishnevkiy.nikita@gmail.com>
2023-10-03 16:44:35 +0300

    1234

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,1 +1,8 @@
-select * from student;
+ALTER TABLE PAYMENT DROP CONSTRAINT payment_type_id_fkey;
+ALTER TABLE PAYMENT DROP CONSTRAINT payment_student_id_fkey;
+ALTER table PAYMENT ADD CONSTRAINT payment_type_id_fkey foreign key (type_id) references paymenttype(id) On delete cascade;
+ALTER table PAYMENT ADD CONSTRAINT payment_student_id_fkey foreign key (student_id) references student(id) On delete cascade;
+ALTER TABLE MARK DROP CONSTRAINT mark_student_id_fkey;
+ALTER TABLE MARK DROP CONSTRAINT mark_subject_id_fkey;
+ALTER TABLE MARK ADD CONSTRAINT mark_student_id_fkey foreign key (student_id) references student(id) on delete cascade;
+ALTER TABLE MARK ADD CONSTRAINT mark_subject_id_fkey foreign key (subject_id) references subject(id) on delete cascade;
