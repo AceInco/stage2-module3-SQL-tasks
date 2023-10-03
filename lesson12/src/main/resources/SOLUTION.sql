969d41076cdb131f5e63ad748306ee9670a4ac91
Mikita <vishnevkiy.nikita@gmail.com>
2023-10-03 16:58:08 +0300

    1234

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 DELETE FROM student WHERE groupnumber >= 4;
 DELETE FROM student WHERE id IN (SELECT m.student_id FROM mark m WHERE m.mark < 4);
-DELETE FROM paymenttype WHERE name = 'Daily';
+DELETE FROM paymenttype WHERE name = 'DAILY';
 DELETE FROM mark WHERE mark < 7;
